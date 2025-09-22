1703334388db1d28d3a0d83b0c849a1cd96d28eb
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2025-09-22 16:06:33 +0530

    Update alu.v

--- single_cycle_riscv/alu.v
+++ single_cycle_riscv/alu.v
@@ -1,3 +1,8 @@
+// alu with overflow 
+
+`timescale 1ns/1ps
+
+
 module alu (
     input  [31:0] a, b,
     input  [2:0]  alu_control,
@@ -7,27 +12,30 @@ module alu (
 
     wire [31:0] sum;
     wire cout;
+    wire slt;
+    wire [31:0] mux1;
     wire carry, overflow, zero, negative;
+    
+    assign mux1 = (alu_control[0] == 1'b0) ? b : ~b; // Mux for ADD/SUB
 
-    // ADD or SUB
-    assign {cout, sum} = (alu_control[0] == 1'b0) ? {1'b0, a} + {1'b0, b} :
-                                             {1'b0, a} + {1'b0, ~b + 1'b1};
+    assign {cout,sum} = a + mux1 + alu_control[0]; // Sum for ADD/SUB
+
+    assign slt = sum[31] ^ overflow; // Set Less Than for SLT
 
     // Result selection
     assign result = (alu_control == 3'b000) ? sum :                  // ADD
                     (alu_control == 3'b001) ? sum :                  // SUB
-                    (alu_control == 3'b010) ? (a & b) :
-                    (alu_control == 3'b011) ? (a | b) :
-                    (alu_control == 3'b101) ? {{31{1'b0}}, ($signed(a) < $signed(b))} :
-                    32'b0;
+                    (alu_control == 3'b010) ? (a & b) :             // AND logical
+                    (alu_control == 3'b011) ? (a | b) :            // OR logical
+                    (alu_control == 3'b101) ? {{31'd0}, slt} : 32'd0; // SLT
 
     // Flags calculation
     assign carry     = (~alu_control[1]) & cout;  
-    assign overflow  = (~alu_control[1]) & ((a[31] == b[31]) && (result[31] != a[31])) & (alu_control[0] == 1'b0);
-    assign zero      = ~( |result );
+    assign overflow  = (~alu_control[1]) & (a[31] ^ sum[31]) & (~(a[31] ^ b[31] ^ alu_control[0]));
+    assign zero      = &(~result);
     assign negative  = result[31];
 
     // Packed Flags: {Negative, Zero, Carry, OverFlow}
-    assign flags = {  overflow, carry,negative, zero};
+    assign flags = {  negative, zero,  carry, overflow};
 
 endmodule
